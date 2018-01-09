433fc0654c3f0efe11c758c5d0c0bdc2ccb27b57
Matthew Paiz <matt@fittech.io>
2018-01-09 11:18:18 -0800

    Overview Properties

--- src/app/projects/experiment/components/project-section/project-section-b.component.ts
+++ src/app/projects/experiment/components/project-section/project-section-b.component.ts
@@ -13,11 +13,20 @@ export class ProjectSectionComponentB {
     @Input()
     content: string 
     @Input()
-    items: [[string, string]]
+    overview: string
 
     constructor() {}
 
     get paragraphs(): string[] {
         return this.content.trim().replace('\r\n','\n').split('\n\n');
+    }
+
+    get points(): [string, string][] {
+        var blocks = this.overview.trim().replace('\r\n','\n').split('\n\n');
+
+        return blocks.map(function(block) {
+            let index = block.indexOf("\n");
+            return [block.substring(0, index), block.substring(index + 1)] as [string, string];
+        });
     }
 }
